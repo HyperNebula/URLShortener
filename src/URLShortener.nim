4860b8c9235b474194b37861d304d0181c72b8a9
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-05-14 16:24:24 -0600

    Added basic httpserver

--- src/URLShortener.nim
+++ src/URLShortener.nim
@@ -2,3 +2,19 @@ import mummy, mummy/routers
 import tiny_sqlite, std/options
 import jsony
 
+
+
+
+proc indexHandler(request: Request) =
+    var headers: HttpHeaders
+    headers["Content-Type"] = "text/plain"
+    request.respond(200, headers, "Hello, World!")
+
+var router: Router
+router.get("/", indexHandler)
+
+let httpServer = newServer(router)
+let port: int = 8080
+
+echo "Serving on http://localhost:" & $port
+httpServer.serve(Port(port))
