ae4ffdaeb57e7a292945f69b1268877b66e844d8
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-06-23 14:39:34 -0400

    Testing post request

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/api")
-def read_root():
-    return {"Hello": "World"}
+class UrlLink(BaseModel):
+    url: str
 
 
-#@app.get("/items/{item_id}")
-#def read_item(item_id: int, q: str | None = None):
+@app.post("/api/addLink")
+def read_link(url_link: UrlLink):
+    url_link_dict = url_link.model_dump()
+
+    return {"Hello": url_link_dict.url}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str | None = None):
 #   return {"item_id": item_id, "q": q}
